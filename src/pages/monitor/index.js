b66d0163874488db220e76dd5b178a1ace559c00
Rana Zohaib <zohaib.dilshad.1@gmail.com>
2023-12-06 21:52:31 +0500

    complete design of a monitor list component

--- src/pages/monitor/index.js
+++ src/pages/monitor/index.js
@@ -1,10 +1,22 @@
 import { useTranslation } from 'react-i18next';
-import { SimpleGrid, Text, Heading, Box, Flex } from '@chakra-ui/react';
+import { SimpleGrid, Text, Heading, Box, Flex, Button, ButtonGroup, IconButton, Checkbox, Spacer, Badge, Tooltip, TableContainer, Table, Tbody } from '@chakra-ui/react';
 import { AppMain } from '@/components/layout/Main';
 import AlertTriangleIcon from '@/icons/alert-triangle';
 import GoodIcon from '@/icons/good';
 import AlertOctagonIcon from '@/icons/alert-octagon';
+import FilterIcon from '@/icons/filter';
+import ListIcon from '@/icons/list-icon';
+import GridIcon from '@/icons/gridIcon';
+import PlayIcon from '@/icons/play';
+import Edit2Icon from '@/icons/edit2';
+import ActionIcon from '@/icons/action-icon';
+import ClockIcon from '@/icons/clock-icon';
+import WebsiteIcon from '@/icons/website';
 import EyesIcon from '@/icons/eye-icon';
+import WeChatIcon from '@/icons/wechat-icon';
+import ShieldIcon from '@/icons/shield-icon';
+import ChromeIcon from '@/icons/chrome-icon';
+import MapIcon from '@/icons/map-icon';
 
 /*
     Notes:
@@ -63,6 +75,89 @@ export default function Monitor() {
                     </Flex>
                 </Box>
             </SimpleGrid>
+            <Flex p={5} mt={5} w="100%" justifyContent="space-between">
+                <Heading>Monitorings</Heading>
+                <Button bg="#1C6DB6" color="white">
+                    New Monitoring
+                </Button>
+            </Flex>
+            <Flex p={5} mt={5} w="100%" justifyContent="space-between">
+                <Button bg="transparent">
+                    <Text px="1">Filter</Text> <FilterIcon width="20px" height="20px" />
+                </Button>
+                <Flex justifyContent="end" alignItems="center">
+                    <Text mr="2" fontWeight={500}>
+                        Appearance
+                    </Text>
+                    <ButtonGroup size="sm" isAttached variant="outline">
+                        <IconButton bg="gray.300" icon={<ListIcon />} />
+                        <IconButton bg="white" icon={<GridIcon />} />
+                    </ButtonGroup>
+                </Flex>
+            </Flex>
+            <TableContainer p={5}>
+                <Table>
+                    <Tbody borderRadius="8px" bg="white" height="72px">
+                        <Flex mw="100" p={5} alignItems="center" height="72px">
+                            <Checkbox p="3"></Checkbox>
+                            <SimpleGrid>
+                                <Heading as="h6" size="xs">
+                                    My first monitoring
+                                    <Tooltip hasArrow label="Phone number" fontSize="md" placement="top">
+                                        <AlertTriangleIcon width="22px" height="22px" color="warning.600" ml="1" />
+                                    </Tooltip>
+                                </Heading>
+                                <Text color="gray.500">2 URLs</Text>
+                            </SimpleGrid>
+
+                            <Spacer />
+                            <Flex justifyContent="space-around" p={4} spacing={2} alignItems="center">
+                                <Badge fontSize="14px" mx={2} bg="warning.50" color="warning.700" px={4} py={1} borderRadius="16px">
+                                    <Flex justifyContent="space-between" alignItems="center" color="warning.700">
+                                        <WebsiteIcon color="warning.700" />
+                                        <Text ml="1">http</Text>
+                                    </Flex>
+                                </Badge>
+                                <Badge fontSize="14px" mx={2} bg="success.50" color="success.700" px={4} py={1} borderRadius="16px">
+                                    <Flex justifyContent="space-between" alignItems="center" color="success.700">
+                                        <WeChatIcon color="success.700" />
+                                        <Text ml="1">WeChat</Text>
+                                    </Flex>
+                                </Badge>
+                                <Badge fontSize="14px" mx={2} bg="orange.50" color="orange.700" px={4} py={1} borderRadius="16px">
+                                    <Flex justifyContent="space-between" alignItems="center" color="orange.700">
+                                        <ShieldIcon color="orange.700" />
+                                        <Text ml="1">QQ</Text>
+                                    </Flex>
+                                </Badge>
+                                <Badge fontSize="14px" mx={2} bg="error.50" color="error.700" px={4} py={1} borderRadius="16px">
+                                    <Flex justifyContent="space-between" alignItems="center" color="error.700">
+                                        <ChromeIcon color="error.700" />
+                                        <Text ml="1">Chrome</Text>
+                                    </Flex>
+                                </Badge>
+                                <Badge fontSize="14px" mx={2} bg="#F8F9FC" color="#F8F9FC" px={4} py={1} borderRadius="16px">
+                                    <Flex justifyContent="space-between" alignItems="center" color="#F8F9FC">
+                                        <MapIcon color="#363F72" />
+                                        <Text ml="1" color="#363F72">
+                                            Country
+                                        </Text>
+                                    </Flex>
+                                </Badge>
+                                <Flex m={8}>
+                                    <ClockIcon />
+                                    <Text color="gray.500" ml={1}>
+                                        30 sec
+                                    </Text>
+                                </Flex>
+                                <IconButton m={1} icon={<PlayIcon />} />
+                                <IconButton m={2} icon={<Edit2Icon />} />
+                                <IconButton m={1} icon={<ActionIcon />} />
+                            </Flex>
+                        </Flex>
+                    </Tbody>
+                </Table>
+            </TableContainer>
         </AppMain>
     );
 }
